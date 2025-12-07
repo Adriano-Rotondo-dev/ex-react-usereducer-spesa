a69e944be0422add745ffe92ca0d324e1f44b5c2
Adriano Rotondo <adriano.rotondo.dev@gmail.com>
2025-12-07 17:43:12 +0100

    Milestone 2 - needs to be revisited

--- src/component/CartComponent.jsx
+++ src/component/CartComponent.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 export default function Cart() {
   const products = [
     { name: "Mela", price: 0.5 },
@@ -5,14 +7,41 @@ export default function Cart() {
     { name: "Latte", price: 1.0 },
     { name: "Pasta", price: 0.7 },
   ];
+
+  const [addedProducts, setAddedProducts] = useState([]);
+
+  const addToCart = (product) => {
+    //prodotto già presente => return
+    if (addedProducts.some((p) => p.name === product.name)) {
+      console.log("Prodotto già presente", product.name);
+      return;
+    }
+    //inserisci prodotto con quantità 1
+    setAddedProducts([...addedProducts, { ...product, quantity: 1 }]);
+    console.log("Prodotti aggiunti:", product.name);
+  };
   return (
     <div className="cart">
       <h3>Carrello dei Prodotti</h3>
       <ul className="cartList">
         {products.map((p, i) => (
-          <li key={i} className="product">
-            {p.name}
+          <li key={i} className="product d-flex">
+            <span className="my-3">{p.name}</span>
+            <button
+              className="mx-5 my-1 btn btn-primary"
+              onClick={() => addToCart(p)}
+            >
+              Aggiungi al carrello
+            </button>
           </li>
+        ))}
+      </ul>
+      <ul className="addedList">
+        {addedProducts.map((p, i) => (
+          <li
+            key={i}
+            className="product"
+          >{`${p.name} - € ${p.price} - ${p.quantity}`}</li>
         ))}
       </ul>
     </div>
